de2984ed3d63d1fc2a715f2e8b976ad8464a267f
Student <student@RU-019.local>
2015-09-14 14:10:58 -0600

    committed

--- main.js
+++ main.js
@@ -8,6 +8,18 @@ var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
 // 	console.log(animals[i])
 // }
 
-for ( var i = animals.length; i >= 0; i--) {
-	console.log(animals[i])
+// for ( var i = animals.length; i >= 0; i--) {
+// 	console.log(animals[i])
+// }
+//  
+for ( var i = 0; i < animals.length; i++) {
+	if (animals[i] >= 3 && animals[i] <=1) {
+		console.log(animals[i * 2])
+	}
+	
+	else {
+		console.log(animals[i])
+	}
 }
+
+// need to get this to repeat spots 1, 2, and 3 for the array. 
