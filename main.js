1a87c4e9a6d9f14a3d48a6a04a5dbd7ac0789f7f
Student <student@RU-019.local>
2015-09-14 12:20:43 -0600

    second done

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
 
-for ( var i = 0; i < animals.length -1; i++){
+// for ( var i = 0; i < animals.length -1; i++){
+// 	console.log(animals[i])
+// }
+
+for (var i = 0; i < animals.length; i+=2) {
 	console.log(animals[i])
 }
